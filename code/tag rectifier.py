582b57c06d549237140d4f23bad3c7c18633225c
unnar19 <unnar19@ru.is>
2023-12-08 18:18:39 +0000

    not much

--- code/tag rectifier.py
+++ code/tag rectifier.py
@@ -3,13 +3,15 @@ import cv2.aruco as aruco
 import numpy as np
 from time import sleep
 
+
 apriltag_dict = aruco.getPredefinedDictionary(aruco.DICT_APRILTAG_36h11)
 param = aruco.DetectorParameters()
 image = cv2.imread('COMP-Final-Project/code/pics/cube_texture.png', cv2.IMREAD_UNCHANGED)
 (img_w,img_h) = (512,512)
 img_corners = np.array([[(0,0), (img_w,0), (img_w, img_h), (0, img_h)]], dtype="float32")
 
-cap = cv2.VideoCapture("COMP-Final-Project\code\pics\cube.mp4")
+path = "COMP-Final-Project\code\pics\cube2.mp4"
+cap = cv2.VideoCapture(0)
 (width,height) = (int(cap.get(3)), int(cap.get(4)))
 
 while(True):
@@ -23,8 +25,6 @@ while(True):
     corners, ids, _ = aruco.detectMarkers(frame, apriltag_dict, parameters=param)
 
 
-
-
     if ids is not None:
         idx = np.argmin(ids)
         corners = np.array([corners[idx]], dtype="float32")
@@ -33,8 +33,13 @@ while(True):
         transformation_matrix = cv2.getPerspectiveTransform(img_corners, corners[0])
         rectified_image = cv2.warpPerspective(image, transformation_matrix, (640, 480))
         not_black = np.all(rectified_image != 0, axis=-1)
-
         frame[not_black] = rectified_image[not_black]
+
+        for i, corner in enumerate(corners[0][0]):
+            cv2.putText(frame,str(i),(int(corner[0]),int(corner[1])),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,255,255),1)
+
+        #aruco.drawDetectedMarkers(frame, corners, ids)
+
 
     cv2.imshow('frame',frame)
 
